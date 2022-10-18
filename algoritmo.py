98d34a4b1e6cd94d49a662032539a53b80900716
lintoeli <lintoeli@gmail.com>
2022-10-18 11:23:45 +0200

    funcion objetivo

--- algoritmo.py
+++ algoritmo.py
@@ -1,6 +1,9 @@
+from cmath import pi, sin, sqrt
 import operator
 import numpy
 import random
+
+#----------------------------------OPERACIONES DE VECTORES Y POBLACION-------------------------------------------------
 
 def calcularDistancias(pesos, vector):
     distanciasEuclideas = {}
@@ -29,7 +32,45 @@ def generarPoblacion(numIndividuos):
             gen = random.random()
             cromosoma.append(gen)
         poblacion.append(cromosoma)
-    return poblacion 
+    return poblacion
+
+#----------------------------------------EVALUACION DE INDIVIDUO------------------------------------------------------
+
+def g(x):
+    suma = 0
+    for i in range(1, len(x)):
+        suma = suma + x[i]
+    return 1 + (9*suma/(len(x) - 1))
+    
+def h(x):
+    f1 = x[0]
+    g = g(x)
+    return 1 - (sqrt(f1/g)) - (f1/g)*sin((10*pi*f1))
+
+def f2(x):
+    return g(x) * h(x)
+
+def z(poblacion):
+    f1 = 100.00
+    f2 = 100.00
+    for x in poblacion:
+        if x[0] < f1:
+            f1 = x[0]
+        if f2(x) < f2:
+            f2 = f2(x)
+    return (f1, f2)
+
+def gte(x, poblacion, pesos): #Funcion a minimizar
+    index = poblacion.index(x)
+    w = pesos[index]
+    z = z(poblacion)
+    f1 = x[0]
+    f2 = f2(x)
+    argumento1 = w[0] * abs(f1 - z[0])
+    argumento2 = w[1] * abs(f2 - z[1])
+    return max(argumento1, argumento2)
+
+#------------------------------------------------INICIALIZACION--------------------------------------------------------
 
 pesos = [
     (0.2,0.8),
@@ -43,6 +84,7 @@ pesos = [
     (0.65,0.35),
     (0.4,0.6)
 ]
+
 #--------------------------------------------PRUEBAS------------------------------------------------------------------
 
 '''
@@ -59,5 +101,10 @@ print("Distancia desde pesos1 a los demas:", pruebaDistancias)
 pruebaVecinos = obtenerVecinos(pesos[1], 3)
 print("Indice de pesos mas cercanos:", pruebaVecinos)
 '''
+'''
+Generar poblacion aleatoria:
+
 poblacion = generarPoblacion(8)
 print(poblacion)
+'''
+
